16a770119c2f5bcc8f2245f8b0b244d951c3c541
Ana0504 <jk455695@users.noreply.github.com>
2020-11-12 19:43:31 +0300

    Initial commit

---
+++ Семестр_5_методы_вычислений_лаба_1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Семестр_5_методы_вычислений_лаба_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            grad_descent_method gdm = new grad_descent_method();
+            int  step=0;
+            double eps = 0.0001, alpha0 = 1, x0 = 0, y0 = 0, dx, dy;          
+            double f0 = gdm.function(x0, y0);
+            dx = Math.Abs(gdm.df_dx(x0, y0));
+            dy = Math.Abs(gdm.df_dy(x0, y0));
+
+            while ((dx >= eps / 2) && (dy >= eps / 2))
+            {
+                gdm.list_df_dx.Add(dx);
+                gdm.list__df_dy.Add(dy);
+                double x1 = gdm.New_coord_x(x0, y0, alpha0);
+                double y1 = gdm.New_coord_y(x0, y0, alpha0);
+                double f1 = gdm.function(x1, y1);
+                if (f0 < f1)
+                {
+                    alpha0 = alpha0 / 2;
+                    x0 = gdm.New_coord_x(x0, y0, alpha0);
+                    y0 = gdm.New_coord_y(x0, y0, alpha0);
+                    f0 = gdm.function(x0, y0);
+                    gdm.list_f_x_y.Add(f0);
+                    gdm.list_x.Add(x0);
+                    gdm.list_y.Add(y0);
+                    gdm.list_alpha.Add(alpha0);
+                    step++;
+                }
+
+                else if (f0 > f1)
+                {
+                    gdm.list_f_x_y.Add(f0);
+                    gdm.list_x.Add(x1);
+                    gdm.list_y.Add(y1);
+                    gdm.list_alpha.Add(alpha0);
+                    f0 = f1; x0 = x1; y0 = y1;
+                    step++;
+                }
+                dx = Math.Abs(gdm.df_dx(x0, y0));
+                dy = Math.Abs(gdm.df_dy(x0, y0));
+            }
+
+            for (int i = 0; i < step; i++)
+            {
+                Console.WriteLine("x= " + gdm.list_x[i] + " " + 
+                                  "y= " + gdm.list_y[i] + " " + 
+                                  "f(x,y)= " + gdm.list_f_x_y[i] + " " + 
+                                  "alpha= " + gdm.list_alpha[i] + " " + 
+                                  "df/dx= " + gdm.list_df_dx[i] + " " + 
+                                  "df/dy= " + gdm.list__df_dy[i] + " ");
+            }
+        }
+        
+    }
+}
---
+++ Семестр_5_методы_вычислений_лаба_1/grad_descent_method.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace Семестр_5_методы_вычислений_лаба_1
+{
+    class grad_descent_method
+    {
+        public List<double> list_x = new List<double>();
+        public List<double> list_y = new List<double>();
+        public List<double> list_f_x_y = new List<double>();
+        public List<double> list_df_dx = new List<double>();
+        public List<double> list__df_dy = new List<double>();
+        public List<double> list_alpha = new List<double>();
+        private double a = 2, b = -1.3, c = 0.04, d= 0.12 ;
+        //private double a = 3, b = -1.2, c = 0.02, d = 0.13;
+
+        public grad_descent_method() 
+        {
+            
+        } 
+        public double New_coord_x(double x, double y, double alpha)
+        {
+            double _x = x - alpha * df_dx(x,y);
+            return _x;
+        }
+
+        public double New_coord_y(double x, double y, double alpha)
+        {
+            double _y = y - alpha * df_dy(x,y);
+            return _y;
+        }
+
+        public double function(double x, double y)
+        {
+            double func = a * x + b* y + Math.Exp(c* x * x + d* y * y);
+            return func;
+        }
+
+        public double end_of_calculate(double x, double y)
+        {
+            double dx = 2 + 0.08 * x * Math.Exp(c * (x * x) + d* y * y);
+            double dy = -1.3 + 0.24 * y * Math.Exp(0.04 * (x * x) + 0.12 * y * y);
+            double df = Math.Pow(Math.Pow(dx, 2) + Math.Pow(dy, 2), 1 / 2);
+            return df;
+        }
+
+        public double df_dx(double x, double y)
+        {
+            double df_dx = a + 2 * c * x * Math.Exp(c * (x * x) + d * y * y);
+            return df_dx;
+        }
+        public double df_dy(double x, double y)
+        {
+            double df_dy = b + 2 * d * y * Math.Exp(c * (x * x) + d * y * y);
+            return df_dy;
+        }
+        
+    }
+}
